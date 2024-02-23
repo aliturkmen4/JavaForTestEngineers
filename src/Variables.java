d7042b55cb23cd10035d5fb860474de1317fb7d2
alitu <alym11731996@gmail.com>
2024-02-23 17:40:51 +0300

    Variables

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void  main(String[] args){
+        int ilkSayi = 10;   // int = Tam sayi demektir
+        System.out.println(ilkSayi);
+
+        String ilkMetin = "Merhaba Otomasyon";
+        System.out.println(ilkMetin);
+
+        System.out.println(ilkSayi+ilkMetin);
+
+        System.out.println("Kursun puani "+ ilkSayi + " ve " + ilkMetin);
+    }
+}
