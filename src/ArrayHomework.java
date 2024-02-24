601dccf39824fa9851eefbc293639f05dc6647a5
alitu <alym11731996@gmail.com>
2024-02-24 08:46:42 +0300

    Array homework

---
+++ src/ArrayHomework.java
@@ -0,0 +1,14 @@
+public class ArrayHomework {
+    public static void main(String[] args) {
+        //3 adet değer içeren bir array oluştur! (Java, Python, C)
+        //1 den 5 e kadar seviye içeren bir array oluştur!
+        //Bu verileri kullanarak Python, Java ve C için seviyeni yazdır!
+
+        String[] dil={"Python", "Java","C"};
+        int[] seviye={1,2,3,4,5};
+
+        System.out.println(dil[0]+" için seviyem: "+seviye[0]);
+        System.out.println(dil[1]+" için seviyem: "+seviye[2]);
+        System.out.println(dil[2]+" için seviyem: "+seviye[1]);
+    }
+}
