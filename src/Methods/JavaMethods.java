e956b1d7c4dcf5aa2185c96c089601277177f0ba
alitu <alym11731996@gmail.com>
2024-02-25 14:36:42 +0300

    Method introduction

---
+++ src/Methods/JavaMethods.java
@@ -0,0 +1,16 @@
+package Methods;
+
+public class JavaMethods {
+    public static void main(String[] args) {
+        konsolaYazdir();
+        yasadigimSehir();
+    }
+
+    private static void konsolaYazdir(){
+        System.out.println("Hello, how are you?");
+    }
+
+    private static void yasadigimSehir(){
+        System.out.println("Antalya");
+    }
+}
