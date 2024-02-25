604cc2255c1726c567eb923f3d50036adcb20588
alitu <alym11731996@gmail.com>
2024-02-26 02:22:22 +0300

    Class & objects

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import OOP.Insan;
+import OOP.Lamba;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -6,5 +7,18 @@ public class Main {
     public static void main(String[] args) {
         Insan ilkInsan=new Insan();
         ilkInsan.konsolaYazdir();
+
+        System.out.println("----------------------------");
+
+        Lamba salonLambasi=new Lamba();
+        salonLambasi.lambaDurumu("Salon");
+
+        Lamba yatakLambasi=new Lamba();
+        yatakLambasi.durumu=true;
+        yatakLambasi.lambaDurumu("Yatak odası");
+        yatakLambasi.durumu=false;
+        yatakLambasi.lambaDurumu("Yatak odası yeni");
+        yatakLambasi.durumu=true;
+        yatakLambasi.lambaDurumu("Yatak odası son durum");
     }
 }
