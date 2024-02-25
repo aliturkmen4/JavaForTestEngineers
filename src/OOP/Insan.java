203dcc25538000135e0e3ea15c2637f422d12711
alitu <alym11731996@gmail.com>
2024-02-26 01:54:23 +0300

    Introduction to oop

---
+++ src/OOP/Insan.java
@@ -0,0 +1,15 @@
+package OOP;
+
+public class Insan {
+    private int boy=180;
+    private int yas=27;
+    private int kilo=90;
+    private String gozRengi="Kahverengi";
+
+    public void konsolaYazdir(){
+        System.out.println("İnsanın boyu: "+boy);
+        System.out.println("İnsanın kilosu: "+kilo);
+        System.out.println("İnsanın yaşı: "+yas);
+        System.out.println("İnsanın göz rengi: "+gozRengi);
+    }
+}
