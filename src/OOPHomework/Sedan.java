904ebde4054f2c34bc6d149ab7bed9c1b8040298
alitu <alym11731996@gmail.com>
2024-02-29 01:01:11 +0300

    OOP homework

---
+++ src/OOPHomework/Sedan.java
@@ -0,0 +1,7 @@
+package OOPHomework;
+
+public class Sedan extends Araba{
+    public Sedan(String model, int kullanilanKm, int fiyat, int kapi) {
+        super("Sedan", model, kullanilanKm, fiyat, kapi);
+    }
+}
