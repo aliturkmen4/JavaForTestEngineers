fe47aca743ca0cc49d69b64585725ab559b7c454
alitu <alym11731996@gmail.com>
2024-02-28 03:59:09 +0300

    Polymorphism

---
+++ src/OOP/Polymorphism.java
@@ -0,0 +1,9 @@
+package OOP;
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        Kedi ilkKedim=new Kedi();
+        ilkKedim.sesCıkar();
+        ilkKedim.uyku();
+    }
+}
