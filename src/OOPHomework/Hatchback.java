93b5c27364e064357f4e9e57cb74f4025bf32a68
alitu <alym11731996@gmail.com>
2024-02-29 01:00:58 +0300

    OOP homework

---
+++ src/OOPHomework/Hatchback.java
@@ -0,0 +1,7 @@
+package OOPHomework;
+
+public class Hatchback extends Araba{
+    public Hatchback(String model, int kullanilanKm, int fiyat, int kapi) {
+        super("Hatchback", model, kullanilanKm, fiyat, kapi);
+    }
+}
