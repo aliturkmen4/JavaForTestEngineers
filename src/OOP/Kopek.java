617918d03e1a0ae48974bd3d92363528859e45da
alitu <alym11731996@gmail.com>
2024-02-29 00:01:26 +0300

    Super keyword usage

--- src/OOP/Kopek.java
+++ src/OOP/Kopek.java
@@ -1,43 +1,23 @@
 package OOP;
 
 public class Kopek extends Hayvan{
-    private String isim;
-    private String cins;
-    private int yas;
 
-    public Kopek(String isim,String cins,int yas){
-        this.isim=isim; //this ile ilk kullandığım değişkenin sınıf içerisinden bir değşken olduğunu belirtiyorum!
-        this.cins=cins;
-        this.yas=yas;
+    //Kopek Hayvan ın subclass ı olduğu için Hayvan daki constructor ı burada etkinleştirdim!
+    public Kopek(String isim, String cins, int yas) {
+        super(isim, cins, yas);
     }
 
     //okuma methodları oluşturuyorum! public olduğu için diğer sınıflar tarafından da kullanılabilecek!
 
-    public String getIsim(){
-        return this.isim;
-    }
-
-    public String getCins(){
-        return this.cins;
-    }
-
-    public int getYas(){
-        return this.yas;
-    }
-
-    //Yazma methodu
-
-    public String setIsim(String isim) {
-       return this.isim = isim;
-    }
 
     @Override
-    void sesCıkar() {
+    void sesCikar() {
         System.out.println("HavHav");
     }
 
     @Override //Hyavan class ın daki methodu override ettiğimi (geçersiz kıldığımı) söylüyorum!
     public void uyku(){
+        super.uyku(); //ana class taki uyku methodunu çağırdım! (ZzZ)
         System.out.println("Köpek uyurken horluyor!");
     }
 }
