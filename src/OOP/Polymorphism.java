81d450d0a8191b92d87dfd730069429a6753fce6
alitu <alym11731996@gmail.com>
2024-02-29 00:01:34 +0300

    Super keyword usage

--- src/OOP/Polymorphism.java
+++ src/OOP/Polymorphism.java
@@ -2,8 +2,9 @@ package OOP;
 
 public class Polymorphism {
     public static void main(String[] args) {
-        Kedi ilkKedim=new Kedi();
-        ilkKedim.sesCıkar();
+        Kedi ilkKedim=new Kedi("Gofret","Sarman",2);
+        ilkKedim.sesCikar();
         ilkKedim.uyku();
+        ilkKedim.komut();
     }
 }
