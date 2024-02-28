a5dc863c422d2a079a706639aeec4113eb3b89f9
alitu <alym11731996@gmail.com>
2024-02-29 00:01:03 +0300

    Super keyword usage

--- src/OOP/Hayvan.java
+++ src/OOP/Hayvan.java
@@ -1,9 +1,36 @@
 package OOP;
 
 abstract class Hayvan {
-    abstract void sesCıkar();
+    protected String isim; //kedi subclass ından erişebilmek için protected yaptım!
+    private String cins;
+    private int yas;
+
+    public Hayvan(String isim,String cins,int yas){
+        this.isim=isim; //this ile ilk kullandığım değişkenin sınıf içerisinden bir değşken olduğunu belirtiyorum!
+        this.cins=cins;
+        this.yas=yas;
+    }
+    public String getIsim(){
+        return this.isim;
+    }
+
+    public String getCins(){
+        return this.cins;
+    }
+
+    public int getYas(){
+        return this.yas;
+    }
+
+    //Yazma methodu
+
+    public String setIsim(String isim) {
+        return this.isim = isim;
+    }
+    abstract void sesCikar();
 
     public void uyku(){
         System.out.println("ZzZ");
     }
+
 }
