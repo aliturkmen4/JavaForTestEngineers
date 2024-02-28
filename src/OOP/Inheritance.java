5d924f598557ac1d7b8740129531c4e23fa072b6
alitu <alym11731996@gmail.com>
2024-02-28 03:36:52 +0300

    Inheritance

---
+++ src/OOP/Inheritance.java
@@ -0,0 +1,12 @@
+package OOP;
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Kopek ilkkopegim=new Kopek("Zeytin","Sivas Kangalı",7);
+        System.out.println("Köpeğimin adı: "+ilkkopegim.getIsim());
+        System.out.println("Köpeğimin cinsi: "+ilkkopegim.getCins());
+        System.out.println("Köpeğimin yaşı: "+ilkkopegim.getYas());
+        ilkkopegim.sesCıkar();
+        ilkkopegim.uyku();
+    }
+}
