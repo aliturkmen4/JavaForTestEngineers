ba3d6d4f7b778d56762b106d4d8cb1ade8caf708
alitu <alym11731996@gmail.com>
2024-03-01 01:06:04 +0300

    Static keyword

---
+++ src/OtomasyonJavaFonksiyonlari/Araba.java
@@ -0,0 +1,15 @@
+package OtomasyonJavaFonksiyonlari;
+
+public class Araba {
+    private String arabaModeli;
+
+    public static int arabaFiyatı=500000;
+
+    public void setArabaModeli(String arabaModeli) {
+        this.arabaModeli = arabaModeli;
+    }
+
+    public String getArabaModeli() {
+        return arabaModeli;
+    }
+}
